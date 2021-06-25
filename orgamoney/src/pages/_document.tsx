e10908fbef82790def2b4bf049554d78eb8ad56a
marrcelosantana <marcelosantana@alu.ufc.br>
2021-06-25 10:32:02 -0300

    First Commit

---
+++ orgamoney/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>OrgaMoney</title> 
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
