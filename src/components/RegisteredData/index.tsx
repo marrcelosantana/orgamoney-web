bccb44865431429ed40be90e40200e3aeb8a9d00
marrcelosantana <marcelosantana@alu.ufc.br>
2021-07-18 14:14:29 -0300

    add component: TableInfo

--- src/components/RegisteredData/index.tsx
+++ src/components/RegisteredData/index.tsx
@@ -1,14 +1,36 @@
 import styles from "./styles.module.scss";
+import TableInfo from "../TableInfo";
 
 export default function RegisteredData() {
   return(
     <div className = { styles.container }>
       <header className = { styles.header }>
         <span>DADOS REGISTRADOS</span>
+         <div className={styles.date}>
+         <select name="month" id="month">
+            <option value="1" disabled selected>Mês</option>
+            <option value="2">Fevereiro</option>
+            <option value="3">Janeiro</option>
+          </select>
+          <select name="year" id="year">
+            <option value="1" disabled selected>Ano</option>
+            <option value="2">2021</option>
+            <option value="3">2020</option>
+          </select>
+        </div>
       </header>
       <div className = { styles.content }>
-          <p>olá mundo</p>
+          <TableInfo />
       </div>
+      {/* <footer className = { styles.footer }>
+        <div className = { styles.info }>
+          <label>ENTRADAS: <span className = { styles.deposit }>R$3000,00</span></label>
+          <label>SAÍDA: <span className = { styles.withdraw }>R$2000,00</span></label>
+        </div>
+        <div className = { styles.total }>
+          <span>TOTAL: +R$1000,00</span>
+        </div>
+      </footer> */}
     </div>
   ); 
 }
@@ -24,15 +46,3 @@ export default function RegisteredData() {
 
 
 
-
-
-{/* <div className={styles.date}>
-  <select name="year" id="year">
-    <option value="1">2021</option>
-    <option value="1">2020</option>
-  </select>
-  <select name="month" id="month">
-    <option value="1">Fevereiro</option>
-    <option value="1">Janeiro</option>
-  </select>
-</div>; */}
