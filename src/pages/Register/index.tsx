1bd830e0fbfd9e2024329d00552c7feac30a4164
marrcelosantana <marcelosantana@alu.ufc.br>
2021-07-21 20:53:40 -0300

    create user

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -3,9 +3,34 @@ import styles from "./register.module.scss";
 import { FiLogIn } from "react-icons/fi";
 import { TiArrowBackOutline } from "react-icons/ti";
 import Link from "next/link";
+import { useHistory } from 'react-router-dom';
 import { InitialLogo } from "../../components/InitialLogo";
+import api from '../../services/api';
+import { useState } from "react";
 
 export default function Register() {
+
+  const [name, setName] = useState('');
+  const [login, setLogin] = useState ('');
+  const [password, setPassword] = useState ('');
+
+  const history = useHistory();
+
+  async function handleRegister(e){
+    e.preventDefault();
+
+    const data = {name, login, password};
+    
+    try{
+      const response = await api.post('/user', data);
+      alert(`Seu ID de acesso: ${ response.data }`)
+      console.log(response);
+      
+    }catch(err){
+      alert('Erro no cadastro, tente novamente!');
+    }
+  }
+
   return (
     <div className={ styles.register }>
       <Head>
@@ -16,19 +41,17 @@ export default function Register() {
           <InitialLogo />
         </div>
         <div className={ styles.menuLogin }>
-          <form className={ styles.form }>
+          <form className={ styles.form } onSubmit = { handleRegister }>
             <h3>CADASTRO</h3>
             <div className={ styles.inputs }>
-              <input type="text" placeholder="Seu Nome" />
-              <input type="email" placeholder="Email@address.com" />
-              <input type="password" placeholder="Senha" />
+              <input type="text" placeholder="Seu Nome"  value = { name } onChange = {e => setName(e.target.value)}/>
+              <input type="email" placeholder="Email@address.com" value = { login } onChange = {e => setLogin(e.target.value)}/>
+              <input type="password" placeholder="Senha" value = { password } onChange = {e => setPassword(e.target.value)}/>
             </div>
-            <Link href="/Main">
-              <button className={ styles.buttonConnect }>
-                <FiLogIn className={ styles.loginIcon }></FiLogIn>
-                CADASTRAR
-              </button>
-            </Link>
+            <button className={ styles.buttonConnect } type="submit">
+              <FiLogIn className={ styles.loginIcon }></FiLogIn>
+              CADASTRAR
+            </button>
             <div className={ styles.options }>
               <Link href="/">
                 <a>
